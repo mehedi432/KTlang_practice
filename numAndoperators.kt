77bf3d56f204715a021f5683d7d5335f4ccdc224
Mehedi Abdullah <aamehedi93@gmail.com>
2018-08-06 22:27:12 +0600

    Create numAndoperators.kt

---
+++ numAndoperators.kt
@@ -0,0 +1,27 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ * Numbers And Operators
+ */
+
+fun main(args: Array<String>){
+	/*
+	 * 
+	 * */	    
+	val a = 12
+    val b = 25.5
+    
+//     Operators
+//  -------------------------------
+//     + - / *
+//     % 
+// 		Main Mathematical operator's
+// 	--------------------------------
+    println(b + a)
+    println(b - a)
+    println(b * a)
+    println(b / a)
+    
+	println(b % a) // will provide the remainder
+    
+}
