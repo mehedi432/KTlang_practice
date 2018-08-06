863527d1671568a1494e752f086d7f72a6df4117
Mehedi Abdullah <aamehedi93@gmail.com>
2018-08-06 20:38:57 +0600

    Create basic.kt

---
+++ basic.kt
@@ -0,0 +1,10 @@
+/**
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ * It is also important that every kotlin file must have a function named
+ * main and that function will expect an Array of string
+*/
+
+fun main(args: Array<String>){
+    println("Heloooo World!")
+}
